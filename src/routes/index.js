20d240ee299cfd2b7ceabd9fb5f4cb1d65c7c932
js <js@js.me>
2020-11-06 14:09:40 +0300

    Update package-lock.json, package.json, and 5 more files...

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const e = require('express');
+var express = require('express');
+var router = express.Router();
+router.get('/', function (req, res, next) {
+    return res.status(200).json({ message: 'Welcome to Express API template' });
+});
+
+module.exports = router;
